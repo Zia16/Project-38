d2fd233444410113d622e0488c4e165c2ccd7907
Zia16 <70327082+Zia16@users.noreply.github.com>
2020-11-22 17:51:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas,background;
+
+var runner;
+
+var gameOver = false;
+
+
+
+function preload(){
+ runnerImg = loadImage("sprites/runner.png");
+
+
+}
+
+
+function setup(){
+createCanvas(840,390);
+runner = createSprite(50,100,25,40);
+runner.depth = 4;
+
+}
+
+
+function draw(){
+
+background(200);
+
+drawSprites();
+
+if(!gameOver){
+    background(200);
+    drawSprites();
+  }
+
+  if(gameOver){
+
+  }
+
+  camera.position.x = runner.position.x + 300;
+
+}
